af3f7a65bc5183f896a0d708cb78640736fe6fe5
antoine <tou-toine@hotmail.fr>
2016-11-24 11:07:36 +0100

    Mise en place du menu paramètre

--- app/src/main/java/com/example/antoine/widgetapplication/MainActivity.java
+++ app/src/main/java/com/example/antoine/widgetapplication/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.antoine.widgetapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.d("menu", "ok");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Log.d("menu", "ok");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
