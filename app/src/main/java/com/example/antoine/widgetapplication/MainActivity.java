95c5dff9c1b99be43109798d8b5b05a2c47fb0cb
antoine <tou-toine@hotmail.fr>
2016-12-08 19:47:07 +0100

    Mise en place du paramétrage de la couleur du widget

--- app/src/main/java/com/example/antoine/widgetapplication/MainActivity.java
+++ app/src/main/java/com/example/antoine/widgetapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.antoine.widgetapplication;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.antoine.settings.SettingsActivity;
+
+import layout.AppWidget;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), AppWidget.class);
+                        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+                        int[] ids = AppWidgetManager.getInstance(getApplication()).getAppWidgetIds(new ComponentName(getApplication(), AppWidget.class));
+                        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+                        sendBroadcast(intent);
+                    }
+                }
+        );
     }
 
     @Override
@@ -34,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Log.d("menu", "click");
+            Log.d("click", "menu");
             lancerParametres();
             return true;
         }
